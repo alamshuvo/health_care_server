100adfc255bcd75170addeaf24ed53d73c0179e5
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-04-09 17:56:12 +0600

    some code added

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,32 +1,54 @@
 import { Prisma, PrismaClient } from "@prisma/client";
+import { adminSearchAbleFields } from "./admin.const";
 
 const prisma = new PrismaClient();
 const getAllAdminService = async (params: any) => {
+  const {searchTerm,...filterData}= params;
   const andConditions:Prisma.AdminWhereInput[] = [];
+  // [
+  //   {
+  //     name: {
+  //       contains: params.searchTerm,
+  //       mode: "insensitive",
+  //     },
+  //   },
+  //   {
+  //     email: {
+  //       contains: params.searchTerm,
+  //       mode: "insensitive",
+  //     },
+  //   },
+  //   {
+  //     contactNumber: {
+  //       contains: params.searchTerm,
+  //       mode: "insensitive",
+  //     },
+  //   },
+  // ],
+
   if (params.searchTerm) {
     andConditions.push({
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-        {
-          contactNumber: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
+      OR:adminSearchAbleFields.map((field)=>{
+        return{
+          [field]:{
+            contains:params.searchTerm,
+            mode:'insensitive',
+          }
+        }
+      })
     });
   }
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+       
+        },
+      })),
+    });
+  }
+  
   const whereConditions:Prisma.AdminWhereInput = {
     AND: andConditions,
   };
