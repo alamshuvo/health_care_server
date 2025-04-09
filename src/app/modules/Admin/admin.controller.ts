b2ee052340083c5b5d9919b9bca376aaffd7ae99
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-04-09 18:13:29 +0600

    some code added

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,18 +1,28 @@
-
 import { Request, Response } from "express";
 import { adminService } from "./admin.service";
+import { adminSearchAbleFields } from "./admin.const";
 
+const pick = <T extends Record<string,unknown>,K extends keyof T>(obj:T, keys:K[]):Partial<T> => {
+  const finalObj:Partial<T> = {};
+  for (const key of keys) {
+    if (obj && Object.hasOwnProperty.call(obj, key)) {
+      finalObj[key] = obj[key];
+    }
+  }
+  return finalObj
+};
 
-const getAllAdmins = async(req:Request,res:Response)=>{
-    const params = req.query;
-const result = await adminService.getAllAdminService(params);
-res.status(200).json({
-    success:true,
-    message:"Admin all data retrived successfully",
-    data:result
-})
-}
+const getAllAdmins = async (req: Request, res: Response) => {
 
-export const adminController ={
-    getAllAdmins
-}
+  const params =   pick(req.query, adminSearchAbleFields);
+  const result = await adminService.getAllAdminService(params);
+  res.status(200).json({
+    success: true,
+    message: "Admin all data retrived successfully",
+    data: result,
+  });
+};
+
+export const adminController = {
+  getAllAdmins,
+};
