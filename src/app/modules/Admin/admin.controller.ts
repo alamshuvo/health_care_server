19340d425575bc134a471956dca3f2703dee5dc0
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-04-13 10:50:43 +0600

    send response added

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,15 +2,18 @@ import { Request, Response } from "express";
 import { adminService } from "./admin.service";
 import { adminFilterAbleFields } from "./admin.const";
 import pick from "../../../shared/pick";
+import sendResponse from "../../../helpers/sendResponse.helper";
 
 const getAllAdmins = async (req: Request, res: Response) => {
   const params = pick(req.query, adminFilterAbleFields);
   const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
 
   const result = await adminService.getAllAdminService(params, options);
-  res.status(200).json({
+
+  sendResponse(res, {
     success: true,
-    message: "Admin all data retrived successfully",
+    statusCode: 200,
+    message: "Admin data retrived successfully",
     meta: result.meta,
     data: result.data,
   });
@@ -19,47 +22,52 @@ const getAllAdmins = async (req: Request, res: Response) => {
 const getSingleAdmin = async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await adminService.getSingleAdminByID(id);
-  res.status(200).json({
+  sendResponse(res,{
     success: true,
+    statusCode: 200,
     message: "Admin data retrived successfully",
     data: result,
-  });
+  })
 };
 
 const updateAdmin = async (req: Request, res: Response) => {
-  const {id}= req.params;
+  const { id } = req.params;
   const data = req.body;
   const result = await adminService.updateAdmin(id, data);
-  res.status(200).json({
+
+  sendResponse(res,{
     success: true,
+    statusCode: 200,
     message: "Admin data updated successfully",
     data: result,
   })
-}
+};
 
-const deleteAdmin = async(req:Request,res:Response)=>{
-  const {id} = req.params;
+const deleteAdmin = async (req: Request, res: Response) => {
+  const { id } = req.params;
   const result = await adminService.deleteAdmin(id);
-  res.status(200).json({
+ sendResponse(res,{
     success: true,
+    statusCode: 200,
     message: "Admin data deleted successfully",
     data: result,
-  })
-}
+ })
+};
 
-const softDeleteAdmin = async(req:Request,res:Response)=>{
-  const {id} = req.params;
+const softDeleteAdmin = async (req: Request, res: Response) => {
+  const { id } = req.params;
   const result = await adminService.deleteSoftAdmin(id);
-  res.status(200).json({
+  sendResponse(res,{
     success: true,
+    statusCode: 200,
     message: "Admin data soft deleted successfully",
     data: result,
   })
-}
+};
 export const adminController = {
   getAllAdmins,
   getSingleAdmin,
   updateAdmin,
   deleteAdmin,
-  softDeleteAdmin
+  softDeleteAdmin,
 };
--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from "express";
 import { userService } from "./user.service";
+import sendResponse from "../../../helpers/sendResponse.helper";
 
 const createAdmin = async (req: Request, res: Response) => {
+ try {
   const data = req.body;
   const result = await userService.createAdmin(data);
-  res.send(result);
+ sendResponse(res,{
+  statusCode:200,
+  success:true,
+  message:"Admin created successfully",
+  data:result
+ })
+ } catch (error) {
+  
+ }
 };
 
 
