1bf6f51600f7b0d3c83b9fcb994e93d69c6297f5
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-04-13 13:09:10 +0600

    jwt accesstoken added

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,9 +1,29 @@
-const loginUser =async (payload:{
-    email:string,
-    password:string
-})=>{
- console.log(payload);
-}
+import { prisma } from "../../../shared/prisma";
+import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const loginUser = async (payload: { email: string; password: string }) => {
+  //check is user data exist
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: payload.email,
+    },
+  });
+  // check is password correct
+  const isCorrectPassword = await bcrypt.compare(
+    payload.password,
+    userData.password
+  );
+  const jwtPayload = {
+    email: userData.email,
+    role: userData.role,
+  };
+  const accessToken = jwt.sign(jwtPayload, "abcdefg", {
+    expiresIn: "15m",
+    algorithm: "HS256",
+  });
+  console.log(accessToken);
+  return userData;
+};
 export const authService = {
-    loginUser
-}
+  loginUser,
+};
