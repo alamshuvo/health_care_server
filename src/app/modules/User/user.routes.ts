1856bd67f2aed3e8990ca93a8a7eab06b74bf437
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-04-21 16:56:22 +0600

    using multer

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,11 +1,14 @@
-import express from 'express';
-import { userController } from './user.controller';
-import auth from '../../middlewares/authMiddleware';
-import { userRole } from '@prisma/client';
+import express from "express";
+import { userController } from "./user.controller";
+import auth from "../../middlewares/authMiddleware";
+import { userRole } from "@prisma/client";
+import { fileUploader } from "../../../helpers/fileUploader";
+
 
 const router = express.Router();
-router.post("/",auth(userRole.ADMIN),userController.createAdmin);
 
 
+
+router.post("/", auth(userRole.ADMIN,userRole.SUPER_ADMIN),fileUploader.upload.single('file'), userController.createAdmin);
 
 export const userRoutes = router;
