75e7f0c666bc628cfb97a73b36f3e0a275571662
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-04-29 12:04:55 +0600

    some code added

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,27 +1,31 @@
-import {addHours, format} from 'date-fns'
+import { addHours, addMinutes, format } from "date-fns";
 const insertIntoDb = async (payload: any) => {
-  const {startDate, endDate, startTime, endTime} = payload;
-  const curentDate = new Date(startDate);
-  const LastDate = new Date(endDate)
- while (curentDate <=LastDate) {
+  const { startDate, endDate, startTime, endTime } = payload;
+  const curentDate = new Date(startDate); // start Date
+  const LastDate = new Date(endDate); // end Date
+
+  const interValTime = 30;
+  while (curentDate <= LastDate) {
     const startDateTime = new Date(
-        addHours(
-            `${format(curentDate,'yyyy-MM-dd')}`,
-            Number(startTime.split(":")[0])
-        )
-    )
+      addHours(
+        `${format(curentDate, "yyyy-MM-dd")}`,
+        Number(startTime.split(":")[0])
+      )
+    );
 
     const endDateTime = new Date(
-        addHours(
-            `${format(LastDate,'yyyy-MM-dd')}`,
-            Number(endTime.split(":")[0])
-        )
-    )
-   while (startDate<= endDateTime ) {
-    
-   }
- }
-
+      addHours(
+        `${format(curentDate, "yyyy-MM-dd")}`,
+        Number(endTime.split(":")[0])
+      )
+    );
+    while (startDateTime < endDateTime) {
+      const scheduleData = {
+        startDateTime: startDateTime,
+        endDateTime: addMinutes(startDateTime, interValTime),
+      };
+    }
+  }
 };
 
 export const scheduleService = {
