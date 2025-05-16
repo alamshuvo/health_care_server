cef0857a229beffa795d7c58fad9deef62c85d3f
alamshuvo <alamshuvozeny019@gmail.com>
2025-05-16 18:10:36 +0600

    error added

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,14 +1,28 @@
-import { NextFunction, Request, Response } from "express"
-import status from "http-status"
+import { Prisma } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import status from "http-status";
 
- const globalErrorHandler=(err:any,req:Request,res:Response,next:NextFunction)=>{
-    res.status(status.INTERNAL_SERVER_ERROR).json({
-     success:false,
-     message:err.name || err.message || "Something went wrong",
-     error:err,
-     stack:err.stack,
-    })
-    next()
- }
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = status.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = err.name || err.message || "Something went wrong";
+  let error = err;
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    message = "validation Error";
+    error = err.message;
+  }
+  res.status(statusCode).json({
+    success,
+    message,
+    error,
+    stack: err.stack,
+  });
+  next();
+};
 
- export default globalErrorHandler
+export default globalErrorHandler;
